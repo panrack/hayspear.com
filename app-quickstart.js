35a0fd8430efa1302a55b2f0750c3cc78092239a
Michael <michaelc@zoovy.com>
2015-01-26 14:12:41 -0800

    logging

--- app-quickstart.js
+++ app-quickstart.js
@@ -1668,7 +1668,7 @@ if(ps.indexOf('?') >= 1)	{
 // if no page content can be determined based on the url, the hash is examined and if appropriately formed, used (ex: #company?show=contact or #category?navcat=.something)
 // should be renamed getPageInfoFromURL
 			detectRelevantInfoToPage : function(URL)	{
-//				app.u.dump("BEGIN myRIA.u.detectRelevantInfoToPage. url: "+URL);
+				app.u.dump("BEGIN myRIA.u.detectRelevantInfoToPage. url: "+URL);
 				var r = {}; //what is returned. r.pageInfo and r.navcat or r.show or r.pid
 				var url = URL; //leave original intact.
 				var hashObj;
@@ -1677,7 +1677,7 @@ if(ps.indexOf('?') >= 1)	{
 					url = tmp[0]; //strip off everything after hash (#)
 					hashObj = this.getPageInfoFromHash(tmp[1]); //will be an object if the hash was a valid pageInfo anchor. otherwise false.
 					}
-
+				dump(url);
 				if(url.indexOf('?') > -1) {
 					var tmp = url.split('?')[1];
 					r.uriParams = tmp; //a simple string of uri params. used to add back onto uri in pushState.
